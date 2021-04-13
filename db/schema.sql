fcbc34d2ad5cd63e63c7863411bbbd490c4dd9f6
Dax Buratto <daxburatto@gmail.com>
2021-04-13 13:49:10 -0500

    added schema

--- db/schema.sql
+++ db/schema.sql
@@ -8,17 +8,24 @@ DROP TABLE IF EXISTS managers;
 DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
-
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-
-);
-
-CREATE TABLE managers (
-
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT UNSIGNED,
+    manager_id INT UNSIGNED,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
